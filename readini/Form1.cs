86c036f266951d485e57a32ab3f40c6196265671
gameyayaya <gameyayaya@gmail.com>
2018-12-14 10:40:43 +0800

    Initial desu!

---
+++ readini/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+
+namespace readini
+{
+    public partial class Form1 : Form
+    {
+        //要从Ini存进来的String , 
+        string DefaultVolume;
+        string HomePage;
+        string Author;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ReadIniToMyApp();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Author);
+        }
+
+        private void ReadIniToMyApp()
+        {
+            //开Ini file
+            var MyIni = new Ini(Application.StartupPath + "\\setting.ini");
+
+            //读Ini 内容进来
+            Author = MyIni.GetValue("Author", "a");
+            DefaultVolume = MyIni.GetValue("DefaultVolume", "a");
+            HomePage = MyIni.GetValue("HomePage", "a");
+            
+
+        }
+
+    }
+
+        
+
+}
